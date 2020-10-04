1ece889ebdce793b122415aaf3ec511cea59c274
AnushkaX <anushka.mutusinghe@gmail.com>
2020-10-04 14:29:28 +0530

    getting quantities from the firebase

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -11,6 +11,7 @@ export class ProductCardComponent {
 
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
+  @Input('shopping-cart') shoppingCart;
 
   constructor(private cartService: ShoppingCartService) { }
 
@@ -18,6 +19,12 @@ export class ProductCardComponent {
 
     this.cartService.addToCart(product);
 
+  }
+
+  getQuantity() {
+    if (!this.shoppingCart) return 0;
+    let item = this.shoppingCart.items[this.product.key];
+    return item ? item.quantity : 0;
   }
 
 
