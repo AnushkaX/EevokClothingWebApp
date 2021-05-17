0bfefb60332adb3156ef552d609b8565f4abfa11
AnushkaX <anushka.mutusinghe@gmail.com>
2021-05-17 12:04:02 +0530

    get download url works

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/observable';
 import { RequestService } from './../request.service';
 import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from 'angularfire2/storage';
 import { finalize } from 'rxjs/operators';
@@ -14,6 +15,7 @@ export class RequestComponent {
   selectedImage: any = null;
   ref: AngularFireStorageReference;
   task: AngularFireUploadTask;
+  downloadURL: any;
 
   constructor(private storage: AngularFireStorage) {}
 
@@ -21,14 +23,21 @@ export class RequestComponent {
   onUpload(event){
     const id = Math.random().toString(36).substring(2);
     this.selectedImage = event.target.files[0];
-    console.log(event);
-    this.ref = this.storage.ref(this.selectedImage.name);
-    
-    this.task = this.ref.put(event.target.files[0]);
   }
 
   upload() {
-
+    this.ref = this.storage.ref(this.selectedImage.name);
+    var name = this.selectedImage.name;
+    const fileRef = this.storage.ref(name);
+    this.ref.put(this.selectedImage).snapshotChanges().pipe(
+      finalize(()=> {
+        fileRef.getDownloadURL().subscribe((url)=>{
+          console.log(url);
+          this.downloadURL = url;
+        })
+      })
+    ).subscribe();
+    
   }
   
 }
